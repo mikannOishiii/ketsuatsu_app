b495f8c62f44de92c93e4bb8606894e9aa222cc3
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-03-18 17:40:42 +0900

    固定ページ作成(トップ・利用規約)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/terms'
+  get '/terms', to: 'static_pages#terms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
