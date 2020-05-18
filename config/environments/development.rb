b72f954dac307b30e34359146db9b0782ddca4c8
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-05-18 12:00:57 +0900

    password reset 機能実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,17 @@ Rails.application.configure do
 
   # devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => Rails.application.credentials.mail[:MAIL_ADDRESS],
+    :password => Rails.application.credentials.mail[:APP_PASSWORD],
+    :authentication => 'login'
+  }
 
   config.require_master_key = true
 end
