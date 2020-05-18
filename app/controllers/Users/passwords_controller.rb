ed5000a06f15815fab686fe20dadfb6f053bad80
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-05-18 12:49:30 +0900

    リダイレクトカスタマイズ

--- app/controllers/Users/passwords_controller.rb
+++ app/controllers/Users/passwords_controller.rb
@@ -21,14 +21,14 @@ class Users::PasswordsController < Devise::PasswordsController
   #   super
   # end
 
-  # protected
+  protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  def after_resetting_password_path_for(resource)
+    Devise.sign_in_after_reset_password ? after_sign_in_path_for(resource) : login_path
+  end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    login_path if is_navigational_format?
+  end
 end
