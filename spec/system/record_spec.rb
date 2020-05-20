b8aeeb916a63cdbda17ecae318620b0671465c2a
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-05-20 17:30:37 +0900

    時々落ちるjs:trueのテストにsleepとrspec-retryを導入

--- spec/system/record_spec.rb
+++ spec/system/record_spec.rb
@@ -13,6 +13,7 @@ RSpec.feature "Record", type: :system do
   scenario "新しいデータの登録に失敗する（日付がない場合）", js: true do
     visit root_path
     click_on "記入する"
+    sleep 0.5
     fill_in "inputDate", with: ""
     fill_in "inputM_SBP", with: 150
     expect(page).not_to have_content "記録する"
@@ -22,28 +23,35 @@ RSpec.feature "Record", type: :system do
   scenario "新しいデータの登録に失敗する（数値以外が入力されている場合）", js: true do
     visit root_path
     click_on "記入する"
+    sleep 0.5
     fill_in "inputDate", with: Time.now
     fill_in "inputM_SBP", with: "hoge"
     click_on "記録する"
+    sleep 0.5
     expect(page).to have_css ".alert-warning"
   end
 
   scenario "新しいデータの登録に成功して、編集に成功する、削除に成功する", js: true do
     visit root_path
     click_on "記入する"
+    sleep 0.5
     fill_in "inputM_SBP", with: 150
     click_on "記録する"
     expect(current_path).to eq root_path
+    sleep 0.5
     expect(page).to have_content "記録を作成しました。"
     click_on "記入する"
+    sleep 0.5
     expect(page).to have_content "150"
     fill_in "inputM_SBP", with: 160
     click_on "記録する"
+    sleep 0.5
     expect(current_path).to eq root_path
     expect(page).to have_content "記録を更新しました。"
     expect(page).to have_content Time.now.strftime("%Y-%m-%d")
     expect(page).to have_content "160"
     click_on "記入する"
+    sleep 0.5
     expect(page).to have_content "160"
     page.accept_confirm do
       click_on "削除する"
@@ -57,13 +65,16 @@ RSpec.feature "Record", type: :system do
   scenario "日付を変更して、データを更新する、データを削除する", js: true do
     visit root_path
     click_on "記入する"
+    sleep 0.5
     fill_in "inputDate", with: Time.now.yesterday
     expect(page).to have_content record_yesterday.m_sbp
     fill_in "inputM_SBP", with: 155
     click_on "記録する"
     expect(current_path).to eq root_path
+    sleep 0.5
     expect(page).to have_content "記録を更新しました。"
     click_on "記入する"
+    sleep 0.5
     fill_in "inputDate", with: Time.now.yesterday
     expect(page).to have_content "155"
     page.accept_confirm do
