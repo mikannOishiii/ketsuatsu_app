577110f2420a96804dfdd1b6135f54a60038a2f7
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-05-22 14:11:12 +0900

    adminユーザーのログイン・ログアウト機能実装

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :admin do
-    
+    account_name { "adminuser" }
+    email { |n| "admin@example.com" }
+    password { "foobar" }
+    password_confirmation { "foobar" }
+
+    trait :invalid do
+      account_name { "" }
+    end
   end
 end
---
+++ spec/requests/admins/sessions_request_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe "Sessions", type: :request do
+  describe 'GET #new' do
+    it "リクエストが成功すること" do
+      get new_admin_session_url
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "POST #create" do
+    let!(:admin) { create(:admin) }
+
+    context "パラメータが妥当な場合" do
+      before do
+        post admin_session_url, params: { admin: attributes_for(:admin) }
+      end
+
+      it "リクエストが成功すること" do
+        expect(response.status).to eq 302
+      end
+
+      it "リダイレクトすること" do
+        expect(response).to redirect_to root_url
+      end
+    end
+
+    context "パラメータが不正の場合" do
+      before do
+        post admin_session_url, params: { admin: attributes_for(:admin, :invalid) }
+      end
+
+      it "リクエストが成功すること" do
+        expect(response.status).to eq 200
+      end
+
+      it "エラーメッセージが表示されること" do
+        expect(response.body).to include "アカウント名またはパスワードが違います。"
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let(:admin) { create(:admin) }
+
+    before do
+      sign_in admin
+    end
+
+    it "リクエストが成功すること" do
+      delete destroy_admin_session_url
+      expect(response.status).to eq 302
+    end
+
+    it "リダイレクトされること" do
+      delete destroy_admin_session_url
+      expect(response).to redirect_to root_url
+    end
+
+  end
+end
