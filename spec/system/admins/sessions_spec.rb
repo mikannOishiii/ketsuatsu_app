f315bad72a7042cdebe04ff000f2201dc58f50e3
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-05-22 14:11:12 +0900

    adminユーザーのログイン・ログアウト機能実装

---
+++ spec/system/admins/sessions_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Admin::Sessions", type: :system do
+  let(:admin) { create(:admin) }
+
+  scenario "ログインに失敗する" do
+    visit new_admin_session_path
+    fill_in "アカウント名", with: admin.account_name
+    fill_in "メールアドレス", with: admin.email
+    fill_in "パスワード", with: ""
+    click_button "ログイン"
+    expect(page).to have_content "アカウント名またはパスワードが違います。"
+    expect(current_path).to eq new_admin_session_path
+  end
+
+  scenario "ログイン・ログアウトに成功する" do
+    visit new_admin_session_path
+    fill_in "アカウント名", with: admin.account_name
+    fill_in "メールアドレス", with: admin.email
+    fill_in "パスワード", with: admin.password
+    click_button "ログイン"
+    expect(page).to have_content "ログインしました。"
+    expect(page).to have_selector "a.nav-link", text: "ログアウト"
+    click_link "ログアウト"
+    expect(page).to have_content "ログアウトしました。"
+    expect(current_path).to eq root_path
+  end
+end
