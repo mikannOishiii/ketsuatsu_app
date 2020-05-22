ee67d1f2c2962bbe2254f1a98200a3d4cb896937
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-05-22 14:44:23 +0900

    adminユーザーのバリデーション設定

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,7 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :account_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
