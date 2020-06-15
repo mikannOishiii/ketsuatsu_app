d10964b37b88252c3190438d614c785293a5c0fd
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-06-15 23:34:58 +0900

    画像のバリデーション追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,15 @@
 class Picture < ApplicationRecord
   belongs_to :post
   mount_uploader :name, PictureUploader
+  validates :name, presence: true
+  validate :picture_size
+
+  private
+
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    if name.size > 1.megabytes
+      errors.add(:name, "1MB以上のファイルはアップロードできません。")
+    end
+  end
 end
